565b8cb1f648014656f157965ea8e83eab758620
HARIS SHAJAN <harisshajanplakkattupalliyil@gmail.com>
2025-07-20 01:51:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let cartCount = 0;
+
+function addToCart() {
+  cartCount++;
+  document.getElementById("cart-count").innerText = cartCount;
+
+  const cartIcon = document.querySelector(".cart");
+  cartIcon.style.color = "#00ff00";
+  setTimeout(() => {
+    cartIcon.style.color = "#fff";
+  }, 300);
+}
